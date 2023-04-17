3e71c79c9bd004923926b669409ef15ccc5af7d1
hilfa vt <hilfavt007@gmail.com>
2023-04-17 15:39:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import uuid from "react-uuid";
+import styles from "./Footer.css";
+import { FiCopy } from "react-icons/fi";
+import { FaInstagram, FaTwitter, FaGithub, FaTelegram } from "react-icons/fa";
+import Sidebar from "./Sidebar";
+import BuildNShip from "./BuildNShip.png";
+import Main from "./Main";
 
 function App() {
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+
+  const [activeNote, setActiveNote] = useState(false);
+
+
+
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled note",
+      body: "Write your note here",
+      lastModified: Date.now(),
+      fontSize:14,
+      textCase:''
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete));
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  const onUpdateNotes = (updatedNote) => {
+    const updatedNotes = notes.map((note) => {
+      if (note.id === activeNote) {
+        return updatedNote;
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+
+      <Main activeNote={getActiveNote()} onUpdateNotes={onUpdateNotes} />
+
+      <div className="footer">
+        <a href="https://buildnship.in/">
+          <img src={BuildNShip} alt="logo" />
         </a>
-      </header>
+        <div className={styles.social_container}>
+          <a href="https://twitter.com/buildnship/">
+            <FaTwitter size={25} color="white" />
+          </a>
+          <a href="https://instagram.com/buildnship?igshid=YmMyMTA2M2Y=">
+            <FaInstagram size={25} color="white" />
+          </a>
+          <a href="https://github.com/BuildNShip">
+            <FaGithub size={25} color="white" />
+          </a>
+          <a href="https://t.me/buildnship">
+            <FaTelegram size={25} color="white" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,182 @@
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from "react-bootstrap/Dropdown";
+import React, { useState, useEffect } from "react";
+import { useRef } from "react";
+
+
+function Main({ activeNote, onUpdateNotes }) {
+  const [textCase, setTextCase] = useState("titlecase");
+  const [fontSize, setFontSize] = useState(14);
+  const [copyStatus, setCopyStatus] = useState("copy");
+
+  const content = useRef(null);
+
+  const toTitleCase = () => {
+    let input = activeNote.body;
+    let output = input
+      .toLowerCase()
+      .replace(/(^\s*\w|[\.\!\?]\s*\w)/g, (s) => s.toUpperCase());
+    return output;
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(activeNote.body);
+    setCopyStatus("copied!");
+    setTimeout(() => {
+      setCopyStatus("copy");
+    }, 2000);
+  };
+
+  const onEditField = (key, val, fsize, fcase) => {
+    onUpdateNotes({
+      ...activeNote,
+      [key]: val,
+      lastModified: Date.now(),
+      fontSize: fsize,
+      textCase: fcase,
+    });
+  };
+
+  const handleFontSizeChange = (size) => {
+    setFontSize(size);
+    onUpdateNotes({
+      ...activeNote,
+      fontSize: size,
+      lastModified: Date.now(),
+    });
+  };
+
+  const handleTextCaseChange = (caseType) => {
+    if (caseType === "titlecase") {
+      caseType = "none";
+      setTextCase(caseType);
+      console.log(content.current.id, content.value);
+      content.value = toTitleCase(activeNote.body);
+      onUpdateNotes({
+        ...activeNote,
+        body: content.value,
+        textCase: caseType,
+        lastModified: Date.now(),
+      });
+      setTextCase('titlecase');
+    } else {
+      setTextCase(caseType);
+      onUpdateNotes({
+        ...activeNote,
+        textCase: caseType,
+        lastModified: Date.now(),
+      });
+    }
+  };
+
+  if (!activeNote) return <div className="no-active-note">No Selected Notes </div> ;
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          className="main-edit-input"
+          id="title"
+          value={activeNote.title}
+          onChange={(e) =>
+            onEditField(
+              "title",
+              e.target.value,
+              activeNote.fontSize,
+              activeNote.textCase
+            )
+          }
+          autoFocus
+        />
+        <div className="app-main-header">
+          {/*  toolbar --------------------------------------- */}
+          <input
+            className="fontsize"
+            type="number"
+            onChange={(e) => handleFontSizeChange(e.target.value)}
+            value={fontSize}
+          />
+
+          {/* dropdown menu-------------------------------------- */}
+          <Dropdown as={ButtonGroup} id="drop-dwn">
+          <Dropdown.Toggle split variant="" value="fxb" className="btn-drop" id="btn-drop">{textCase}</Dropdown.Toggle>
+            <Dropdown.Menu id="menu-drop">
+              <Dropdown.Item
+                id="menu-item" value="none" text="none"
+                onClick={() => handleTextCaseChange("none")}
+              >
+                None
+              </Dropdown.Item>
+              <Dropdown.Item
+                id="menu-item" value="upper"
+                onClick={() => handleTextCaseChange("uppercase")}
+              >
+                Upper
+              </Dropdown.Item>
+              <Dropdown.Item
+                id="menu-item" value="lower"
+                onClick={() => handleTextCaseChange("lowercase")}
+              >
+                Lower
+              </Dropdown.Item>
+              <Dropdown.Item
+                id="menu-item" value="capitalize"
+                onClick={() => handleTextCaseChange("capitalize")}
+              >
+                Capital
+              </Dropdown.Item>
+              <Dropdown.Item
+                id="menu-item" value="title"
+                onClick={() => handleTextCaseChange("titlecase")}
+              >
+                Sentence
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          
+          </Dropdown>
+          <button className="btn-copy btn-white" onClick={handleCopy}>
+            {copyStatus}
+          </button>
+        </div>
+
+        <textarea
+          id="body"
+          ref={content}
+          placeholder="write your note here......"
+          style={{
+            fontSize: `${activeNote.fontSize}px`,
+            textTransform: `${activeNote.textCase}`,
+          }}
+
+          value={
+            activeNote.textCase === "titlecase" ||
+            activeNote.textCase === "sentencecase"
+              ? toTitleCase(activeNote.body)
+              : activeNote.body
+          }
+
+          onChange={(e) =>
+            onEditField(
+              "body",
+              e.target.value,
+              activeNote.fontSize,
+              activeNote.textCase
+            )
+          }
+
+          
+        />
+
+        <div className="textarea-footer">
+          <i className="words">
+            words : {activeNote.body.trim().split(/\s+/).length}
+          </i>
+          <i className="chars">characters : {activeNote.body.length}</i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
